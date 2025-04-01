aa6e03e7a2ee84bccb29e94a4027496e76b1a5cf
100495956 <100495956@alumnos.uc3m.es>
2025-04-01 13:26:00 +0200

    Implementation of mygrep.c (first iteration)

--- mygrep.c
+++ mygrep.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_LINESIZE 500
+#define MAX_LINENUMB 30
 
 int main(int argc, char ** argv) {
     // search for text string in a file and displays the lines where that str appears
@@ -16,5 +18,33 @@ int main(int argc, char ** argv) {
         return -1;
     }
 
+	//Set up
+	char buffer[MAX_LINESIZE];
+	char in_lines[MAX_LINESIZE*MAX_LINENUMB] = "";
+	char string[strlen[argv[2]]] = argv[2];
+	int file = open(argv[1],O_RDONLY);
+	
+	if (file < 0) {
+        perror("Error opening file");
+        return -1;
+	}
+	
+	//Main function
+	while not feof(file){ //fgets could be used directly but feof is used for clarity 
+		fgets(buffer,MAX_LINESIZE,file);
+		if (buffer == string) {//current line = given line
+			printf("%s\n",buffer)
+			in_lines = strcat(in_lines, buffer) //store ocurrence in in_lines
+		}
+		//continue
+	}
+	
+	if in_lines == "" {//no line was concatenated after reading the file
+		printf("%s not found.\n",string);
+		close(file);
+		return 0;
+	}
+	
+	close(file);
     return 0;
 }
