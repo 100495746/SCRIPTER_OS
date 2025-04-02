2e9b4901fa667e5a3ed51d18b78097886ee2bebf
100495956 <100495956@alumnos.uc3m.es>
2025-04-02 21:11:35 +0200

    Final revision mygrep.c

--- mygrep.c
+++ mygrep.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LINENUMB 30
+#define MAX_LINESIZE 512
 
 int main(int argc, char ** argv) {
     // search for text string in a file and displays the lines where that str appears
@@ -17,10 +17,10 @@ int main(int argc, char ** argv) {
     	}
 
 	//Set up
-	char buffer[strlen(argv[2]) + 1];
+	char buffer[MAX_LINESIZE];
 	char string[strlen(argv[2]) + 1];
 	strcpy(string, argv[2]);
-	int occurrence = 0;//Variable stores number of times string is found
+	int instance = 0;//Variable stores number of times string is found
 	FILE *file = fopen(argv[1], "r");
 	//Error handling
 	if (!file) {
@@ -28,13 +28,13 @@ int main(int argc, char ** argv) {
         return -1;
 	}
 	//Main function
-	while (fgets(buffer,strlen(argv[2]) + 1,file)){
+	while (fgets(buffer,MAX_LINESIZE,file)){
 		if (strstr(buffer, string)) {//Current line = given line
 			printf("%s",buffer);
-			occurrence = occurrence++;
+			instance = instance++;
 		}
 	}
-	if (ocurrence == 0) {//No line was concatenated after reading the file
+	if (instance == 0) {//No line was concatenated after reading the file
 		printf("%s not found.\n",string);
 	}
 	fclose(file);
