e757322acac56a6993f8a9932ea6c1ca42d3ec73
100495746 <100495746@alumnos.uc3m.es>
2025-04-03 17:02:01 +0200

    Funcional menos en una parte, preadaptación

---
+++ ssoo_p2_tester/ssoo_p2_100495956_100495810_100495746/mygrep.c
@@ -0,0 +1,45 @@
+
+	
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINESIZE 512
+
+int main(int argc, char ** argv) {
+    // search for text string in a file and displays the lines where that str appears
+    //On error, it gives: perror ("text describing error) and returns -1") Also,
+    //If the str is not found, it will display the message "%s not found \n"
+
+
+    // 3 args are expected no more, no less
+    	if (argc != 3) {
+        	printf("Usage: %s <ruta_fichero> <cadena_busqueda>\n", argv[0]);
+        	return -1;
+    	}
+
+	//Set up
+	char buffer[MAX_LINESIZE];
+	char string[strlen(argv[2]) + 1];
+	strcpy(string, argv[2]);
+	int instance = 0;//Variable stores number of times string is found
+	FILE *file = fopen(argv[1], "r");
+	//Error handling
+	if (!file) {
+        perror("Error opening file");
+        return -1;
+	}
+	//Main function
+	while (fgets(buffer,MAX_LINESIZE,file)){
+		if (strstr(buffer, string)) {//Current line = given line
+			printf("%s",buffer);
+			instance = instance++;
+		}
+	}
+	if (instance == 0) {//No line was concatenated after reading the file
+		printf("%s not found.\n",string);
+	}
+	fclose(file);
+	return 0;
+}
